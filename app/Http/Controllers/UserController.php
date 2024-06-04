e871a60667a65ea954de37bafb164bdcd5345f8d
Pardeep549 <148062556+Pardeep549@users.noreply.github.com>
2024-06-04 18:26:08 +0530

    shopjson

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function main()
+    {
+        return view('layouts.main');
+    }
+}
