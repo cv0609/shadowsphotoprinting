eda7393c46021e0134c0fc4b01fc9ba1d6775d3d
Pardeep549 <148062556+Pardeep549@users.noreply.github.com>
2024-06-04 16:01:32 +0530

    new json

--- app/helpers.php
+++ app/helpers.php
@@ -2,9 +2,11 @@
 
 function read_json($file_name = ''){
     $pointer = base_path('resources/pages_json/'.$file_name);
-    if(file_exists($pointer)){
-        $data = json_decode(file_get_contents($pointer));
-    }else{
+   if(file_exists($pointer)){
+    dd(json_decode(file_get_contents($pointer),true));
+    $data = json_decode(file_get_contents($pointer));
+   
+   }else{
         $data = null;
     }
     return $data;
