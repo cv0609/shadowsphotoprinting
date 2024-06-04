07f0e6b64d5b572e212d8437ca76595e16d3f912
nd@int.iqsay.com <nd@int.iqsay.com>
2024-06-04 18:34:11 +0530

    fixed

--- app/Http/Controllers/admin/PagesController.php
+++ app/Http/Controllers/admin/PagesController.php
@@ -38,6 +38,44 @@ class PagesController extends Controller
 
     public function update(Request $request)
      {
-         dd($request->all());
+        $page_fields_data = read_json(strtolower($page->slug) . '.json');
+
+        $sections = $page_fields_data->sections;
+        foreach ($sections as $section) {
+            $fields = $section->fields;
+
+            foreach ($fields as $field) {
+                $field_name = $field->name;
+                $field_value = "";
+                if($field->type =='multiselect')
+                {
+                    if (preg_match('/^([^\[]+)/', $field->name, $matches)) {
+                        // $matches[1] contains the matched part
+                        $field_name = $matches[1];
+
+                    }
+                }
+                if (($field->type == 'image' || $field->type == 'video' || $field->type == 'file')) {
+                    $file = $request->$field_name;
+
+                    if (!empty($file)) {
+
+                        $uploadFileName = $this->uploadService->handleUploadedImages($file, $this->flie_upload_path, $this->availableImageExtensions);
+
+                        if ($uploadFileName != "") {
+                            $field_value = $uploadFileName;
+                        }
+                    } else if (array_key_exists($field_name, $existing_page_content)) {
+
+                        $field_value = $existing_page_content[$field_name];
+                    } else {
+                        //todo
+                    }
+                } else {
+                    $field_value = $request->$field_name;
+                }
+                $pageContent[$field->name] = $field_value;
+            }
+        }
      }
 }
