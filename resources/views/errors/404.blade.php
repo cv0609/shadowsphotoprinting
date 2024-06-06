5b9415ba3f250ed689825d7744ed343b160113d9
nishant <nd@int.iqsay.com>
2024-06-06 11:30:01 +0530

    fixed

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,9 @@
     <title>404 Not Found</title>
 </head>
 <body>
-    <h1>404 Not Found</h1>
-    <p>The page you requested could not be found.</p>
+    <div class="page-not-found">
+        <h1>404 Not Found</h1>
+        <p>The page you requested could not be found.</p>
+    </div>
 </body>
 </html>
