e3a9ae9469343e7b1d495ce790899d935d4e8d15
Pardeep549 <148062556+Pardeep549@users.noreply.github.com>
2024-06-11 18:11:07 +0530

    fixed

--- app/Http/Controllers/admin/BlogsController.php
+++ app/Http/Controllers/admin/BlogsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\admin;
 use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
