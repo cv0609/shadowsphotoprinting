3d9b017e6c7eba1ff849d9a1bd027558d13bd1ed
Pardeep549 <148062556+Pardeep549@users.noreply.github.com>
2024-06-19 15:40:46 +0530

    fix

--- database/migrations/2024_06_14_063709_create_coupons_table.php
+++ database/migrations/2024_06_14_063709_create_coupons_table.php
@@ -16,10 +16,10 @@ class CreateCouponsTable extends Migration
         if (!Schema::hasTable('coupons')) {
             Schema::create('coupons', function (Blueprint $table) {
                 $table->id();
-                $table->enum("object_type", [0 , 1])->default("no")->change();
+                $table->enum("object_type", ['0','1'])->default("0")->change();
                 $table->integer("vendor_id")->nullable();
                 $table->string('code');
-                $table->enum("type", [0 , 1])->default("no")->change();
+                $table->enum("type", ['0','1'])->comment('0 for amount, 1 for percent')->default("0")->change();
                 $table->double('amount', 12, 2);
                 $table->double('minimum_spend', 12, 2)->nullable();
                 $table->double('maximum_spend', 12, 2)->nullable();
@@ -29,7 +29,7 @@ class CreateCouponsTable extends Migration
                 $table->integer("same_ip_limit")->nullable();
                 $table->integer("use_limit_per_user")->nullable();
                 $table->string("use_device")->nullable();
-                $table->enum("multiple_use", [0 , 1])->default("no")->change();
+                $table->enum("multiple_use", ['0','1'])->comment('0 for No, 1 for Yes')->default("0")->change();
                 $table->integer("total_use")->default(0);
                 $table->integer("status")->default(0);
                 $table->timestamps();
