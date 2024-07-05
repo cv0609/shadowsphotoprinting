cde30010228a9cb5e95c8915ff4da096dbd20fd4
nd@int.iqsay.com <nd@int.iqsay.com>
2024-07-05 16:15:50 +0530

    fixed

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,6 +9,7 @@ use App\Models\Country;
 use App\Models\Coupon;
 use App\Models\Shipping;
 use App\Models\State;
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Services\CartService;
@@ -125,6 +126,8 @@ class CartController extends Controller
      {
        $coupon = Coupon::where('code', $request->coupon_code)->first();
         $total = $this->CartService->getCartTotal();
+        $product = Product::find($request->product_id);
+        $productCategories = $product->categories->pluck('id')->toArray();
 
         if (!$coupon) {
             return ['success' => false, 'message' => 'Coupon does not exist'];
@@ -152,6 +155,14 @@ class CartController extends Controller
 
         if ($coupon->use_limit && $coupon->total_use >= $coupon->use_limit) {
             return ['success' => false, 'message' => 'This coupon has reached its usage limit.' ];
+        }
+
+        if (!empty($coupon->categories) && !array_intersect($productCategories, explode(',',$coupon->categories))) {
+            return back()->withErrors(['code' => 'This coupon is not applicable to the selected product\'s category.']);
+        }
+
+        if (!empty($coupon->products) && !in_array($product->id, explode(',',$coupon->products))) {
+            return back()->withErrors(['code' => 'This coupon is not applicable to the selected product.']);
         }
 
         $amount = 0;
