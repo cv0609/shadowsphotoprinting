6eabb1894f126896bbde814e36e473e76b8744ca
nd@int.iqsay.com <nd@int.iqsay.com>
2024-07-22 17:36:55 +0530

    fixed

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -1,6 +1,13 @@
 @extends('admin.layout.main')
 @section('page-content')
 <div class="right_col" role="main" style="min-height: 3963px;">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+          <li class="breadcrumb-item"><a href="{{ route('product-list') }}">Products</a></li>
+          <li class="breadcrumb-item"><a href="#">Edit Product</a></li>
+        </ol>
+      </nav>
 <div class="">
 <div class="row">
     <div class="col-md-12 col-sm-12 ">
