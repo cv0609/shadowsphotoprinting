64ef7bb8f5640bd1d77936ff32552d9a9d422b30
Pardeep549 <148062556+Pardeep549@users.noreply.github.com>
2024-07-23 11:26:27 +0530

    fixed

--- resources/views/admin/orders/order_details.blade.php
+++ resources/views/admin/orders/order_details.blade.php
@@ -13,12 +13,162 @@
       </nav>
     <div class="row">
       <div class="col-md-12">
+        <div class="x_panel">
+          <div class="order-address">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="gerneral_billing_details">
+                  <h4 class="mb-3">General</h4>
+
+                  <div class="main_input_div">
+                <label for="">Date created:</label>
+                <p>2024-7-23</p>
+                  </div>
+
+
+                  <div class="main_input_div">
+                    <label for="">Status : </label>
+                    <select class="form-select form-control" aria-label="Default select example">
+                      <option selected>Processing</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+
+
+                  <div class="main_input_div">
+                    <label for="">Jenny Garley : </label>
+                    <select class="form-select form-control" aria-label="Default select example">
+                      <option selected>Jenny Garley</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+
+
+
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="order-address-details">
+                    <h4 class="mb-3">Billing details</h4>
+                <ul class="m-0 list-unstyled">
+                <li>
+                  <h6>First name </h6>
+                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['fname']) }}</p>
+                </li>
+                <li>
+                  <h6>Last name</h6>
+                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['lname']) }}</p>
+                </li>
+                <li>
+                  <h6>Company name</h6>
+                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['lname']) }}</p>
+                </li>
+                <li>
+                  <h6>Country / Region</h6>
+                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['lname']) }}</p>
+                </li>
+                <li>
+                  <h6>Street address</h6>
+                  <p>{{ $orderDetail->OrderBillingDetail['street1'].','.$orderDetail->OrderBillingDetail['street2'] }}</p>
+                </li>
+                <li>
+                  <h6>Suburb</h6>
+                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['suburb']) }}</p>
+                </li>
+                <li>
+                  <h6>State</h6>
+                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['state']) }}</p>
+                </li>
+                <li>
+                  <h6>Postcode</h6>
+                  <p>{{ $orderDetail->OrderBillingDetail['postcode'] }}</p>
+                </li>
+                <li>
+                  <h6>Phone</h6>
+                  <p>{{ $orderDetail->OrderBillingDetail['phone'] }}</p>
+                </li>
+                <li>
+                  <h6>Email address</h6>
+                  <p>{{ $orderDetail->OrderBillingDetail['email'] }}</p>
+                </li>
+                <li>
+                  <h6>Account username</h6>
+                  <p>{{ $orderDetail->OrderBillingDetail['username'] }}</p>
+                </li>
+                <li>
+                  <h6>Account password</h6>
+                  <p>{{ $orderDetail->OrderBillingDetail['password'] }}</p>
+                </li>
+              </ul>
+                </div>
+
+              </div>
+              <div class="col-md-4">
+                @if($orderDetail->isShippingAddress)
+                    <div class="order-address-details" id="Shipping-address">
+                        <h4 class="mb-3">Shipping details</h4>
+                        <ul class="m-0 list-unstyled">
+                            <li>
+                            <h6>First name </h6>
+                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_fname'])}} </p>
+                            </li>
+                            <li>
+                            <h6>Last name</h6>
+                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_lname'])}}</p>
+                            </li>
+                            <li>
+                            <h6>Company name</h6>
+                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_company'])}}</p>
+                            </li>
+                            <li>
+                            <h6>Country / Region</h6>
+                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_country_region'])}}</p>
+                            </li>
+                            <li>
+                            <h6>Street address</h6>
+                            <p>{{$orderDetail->OrderBillingDetail['ship_street1'].','.$orderDetail->OrderBillingDetail['ship_street2']}}</p>
+                            </li>
+                            <li>
+                            <h6>Suburb</h6>
+                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_suburb'])}}</p>
+                            </li>
+                            <li>
+                            <h6>State</h6>
+                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_state'])}}</p>
+                            </li>
+                            <li>
+                            <h6>Postcode</h6>
+                            <p>{{$orderDetail->OrderBillingDetail['ship_state']}}</p>
+                            </li>
+                            <li>
+                            <h6>Order notes</h6>
+                            <p>{{$orderDetail->OrderBillingDetail['order_comments']}}</p>
+                            </li>
+                        </ul>
+                    </div>
+                @else
+                <div class="diffrent-address">
+                  <h4 class="mb-3">Shipping details</h4>
+                  <p>The shipping address is the same as the billing address.</p>
+                </div>
+                @endif
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12">
           <div class="x_panel">
       <div class="x_title">
         <h2>Order Details</h2>
         <div class="clearfix"></div>
       </div>
       <div class="x_content">
+
       <div class="table-responsive-sm order-invoice-main">
         <table class="table table-striped">
           <thead>
@@ -131,120 +281,7 @@
       </div>
     </div>
       </div>
-      <div class="col-md-12">
-        <div class="x_panel">
-          <div class="order-address">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="order-address-details">
-                    <h4 class="mb-3">Billing details</h4>
-                <ul class="m-0 list-unstyled">
-                <li>
-                  <h6>First name </h6>
-                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['fname']) }}</p>
-                </li>
-                <li>
-                  <h6>Last name</h6>
-                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['lname']) }}</p>
-                </li>
-                <li>
-                  <h6>Company name</h6>
-                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['lname']) }}</p>
-                </li>
-                <li>
-                  <h6>Country / Region</h6>
-                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['lname']) }}</p>
-                </li>
-                <li>
-                  <h6>Street address</h6>
-                  <p>{{ $orderDetail->OrderBillingDetail['street1'].','.$orderDetail->OrderBillingDetail['street2'] }}</p>
-                </li>
-                <li>
-                  <h6>Suburb</h6>
-                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['suburb']) }}</p>
-                </li>
-                <li>
-                  <h6>State</h6>
-                  <p>{{ Str::ucfirst($orderDetail->OrderBillingDetail['state']) }}</p>
-                </li>
-                <li>
-                  <h6>Postcode</h6>
-                  <p>{{ $orderDetail->OrderBillingDetail['postcode'] }}</p>
-                </li>
-                <li>
-                  <h6>Phone</h6>
-                  <p>{{ $orderDetail->OrderBillingDetail['phone'] }}</p>
-                </li>
-                <li>
-                  <h6>Email address</h6>
-                  <p>{{ $orderDetail->OrderBillingDetail['email'] }}</p>
-                </li>
-                <li>
-                  <h6>Account username</h6>
-                  <p>{{ $orderDetail->OrderBillingDetail['username'] }}</p>
-                </li>
-                <li>
-                  <h6>Account password</h6>
-                  <p>{{ $orderDetail->OrderBillingDetail['password'] }}</p>
-                </li>
-              </ul>
-                </div>
-
-              </div>
-              <div class="col-md-6">
-                @if($orderDetail->isShippingAddress)
-                    <div class="order-address-details" id="Shipping-address">
-                        <h4 class="mb-3">Shipping details</h4>
-                        <ul class="m-0 list-unstyled">
-                            <li>
-                            <h6>First name </h6>
-                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_fname'])}} </p>
-                            </li>
-                            <li>
-                            <h6>Last name</h6>
-                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_lname'])}}</p>
-                            </li>
-                            <li>
-                            <h6>Company name</h6>
-                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_company'])}}</p>
-                            </li>
-                            <li>
-                            <h6>Country / Region</h6>
-                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_country_region'])}}</p>
-                            </li>
-                            <li>
-                            <h6>Street address</h6>
-                            <p>{{$orderDetail->OrderBillingDetail['ship_street1'].','.$orderDetail->OrderBillingDetail['ship_street2']}}</p>
-                            </li>
-                            <li>
-                            <h6>Suburb</h6>
-                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_suburb'])}}</p>
-                            </li>
-                            <li>
-                            <h6>State</h6>
-                            <p>{{Str::ucfirst($orderDetail->OrderBillingDetail['ship_state'])}}</p>
-                            </li>
-                            <li>
-                            <h6>Postcode</h6>
-                            <p>{{$orderDetail->OrderBillingDetail['ship_state']}}</p>
-                            </li>
-                            <li>
-                            <h6>Order notes</h6>
-                            <p>{{$orderDetail->OrderBillingDetail['order_comments']}}</p>
-                            </li>
-                        </ul>
-                    </div>
-                @else
-                <div class="diffrent-address">
-                  <h4 class="mb-3">Shipping details</h4>
-                  <p>The shipping address is the same as the billing address.</p>
-                </div>
-                @endif
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+  
     </div>
   </div>
 @endsection
