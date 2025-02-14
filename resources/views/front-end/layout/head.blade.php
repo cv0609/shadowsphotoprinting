613a21bf628656f92787572d944e4b84f6f63211
Avology <dharmendraavology@gmail.com>
2025-02-14 17:15:25 +0530

    added

--- resources/views/front-end/layout/head.blade.php
+++ resources/views/front-end/layout/head.blade.php
@@ -5,6 +5,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name='robots' content='{{$index=='1' ? config('constant.dashboard_index.on') : config('constant.dashboard_index.off')}}' />
+<meta name="google-site-verification" content="QscuU-oHKOGqK2i0FisB3Hzy7Teuqry8iBl2TaWjFyM" />
 {{-- <title>{{ $page_content['meta_title'] ?? 'Default Title' }}</title> --}}
 <title>{{ html_entity_decode(ucfirst($page_content['meta_title']) ?? 'Default Title') }}</title>
 <meta name="description" content="{{ $page_content['meta_description'] ?? 'Default Description' }}">
@@ -20,3 +21,37 @@
 <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.1/css/select2.min.css" rel="stylesheet" />
 <script src="https://js.stripe.com/v3/"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-27QW0FL5D9"></script>
+<script>
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+ 
+   gtag('config', 'G-27QW0FL5D9');
+ </script>
+
+<noscript>
+   <img
+      height="1"
+      width="1"
+      style="display:none"
+      alt="fbpx"
+      src="https://www.facebook.com/tr?id=3392352141053321&ev=PageView&noscript=1"
+   />
+</noscript>
+
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+   })(window,document,'script','dataLayer','GTM-TC2ZRBMT');</script>
+   <!-- End Google Tag Manager -->
+
+   <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TC2ZRBMT"
+   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+   <!-- End Google Tag Manager (noscript) -->
+   
+   
+
+ 
