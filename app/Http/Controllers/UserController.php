229d82945839e0ffc87deb800952b5ce1ab7da6c
karan <kn@int.iqsay.com>
2025-02-21 13:09:01 +0530

    fixed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
     public function index()
      {
-        $wptable = DB::table('wp_users')->all();
+        $wptable = DB::table('wp_users')->get();
         dd($wptable);
         $user = User::insert(['username'=>$request->name,'email'=>$request->email,'password'=>$hashedPassword]);
 
