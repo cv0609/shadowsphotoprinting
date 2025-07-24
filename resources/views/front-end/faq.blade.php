bd739f40e18f11f2a53fab37407bd492ba8b2bc1
nd@int.iqsay.com <nd@int.iqsay.com>
2025-07-24 16:29:56 +0530

    faq bugs fixed

--- resources/views/front-end/faq.blade.php
+++ resources/views/front-end/faq.blade.php
@@ -8,25 +8,44 @@
         </div>
     </div>
 </section>
- @foreach($items as $group)
+@php
+    $globalIndex = 0; // Global index to track first item overall
+@endphp
+
+@foreach($items as $groupIndex => $group)
 <div class="container mt-5 mb-5">
     <div class="faq-section-title">{{ $group['main_heading'] }}</div>
-    <div class="accordion" id="accordionExample">
+    <div class="accordion" id="accordionGroup{{ $groupIndex }}">
         @foreach($group['faqs'] as $index => $item)
+        @php
+            $isFirst = $globalIndex === 0;
+        @endphp
         <div class="accordion-item">
-            <h2 class="accordion-header" id="heading{{ $index }}">
-                <button class="accordion-button {{ $index != 0 ? 'collapsed' : '' }}" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $index }}" aria-expanded="{{ $index == 0 ? 'true' : 'false' }}" aria-controls="collapse{{ $index }}">
+            <h2 class="accordion-header" id="heading{{ $groupIndex }}-{{ $index }}">
+                <button class="accordion-button {{ !$isFirst ? 'collapsed' : '' }}"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapse{{ $groupIndex }}-{{ $index }}"
+                        aria-expanded="{{ $isFirst ? 'true' : 'false' }}"
+                        aria-controls="collapse{{ $groupIndex }}-{{ $index }}">
                     {{ $item['title'] }}
                 </button>
             </h2>
-            <div id="collapse{{ $index }}" class="accordion-collapse collapse {{ $index == 0 ? 'show' : '' }}" aria-labelledby="heading{{ $index }}" data-bs-parent="#accordionExample">
+            <div id="collapse{{ $groupIndex }}-{{ $index }}"
+                 class="accordion-collapse collapse {{ $isFirst ? 'show' : '' }}"
+                 aria-labelledby="heading{{ $groupIndex }}-{{ $index }}"
+                 data-bs-parent="#accordionGroup{{ $groupIndex }}">
                 <div class="accordion-body">
                     {{ $item['content'] }}
                 </div>
             </div>
         </div>
+        @php
+            $globalIndex++;
+        @endphp
         @endforeach
     </div>
 </div>
- @endforeach
+@endforeach
+
 @endsection
