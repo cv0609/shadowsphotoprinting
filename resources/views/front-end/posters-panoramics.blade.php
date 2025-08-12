90b56dd19be575df6c2e6c67d7fac61a58f53ce6
dev-avology <devavology12@gmail.com>
2025-08-12 16:20:54 +0530

    remove show more option

--- resources/views/front-end/posters-panoramics.blade.php
+++ resources/views/front-end/posters-panoramics.blade.php
@@ -59,7 +59,7 @@
                                     <option>{{ $product['type_of_paper_use'] }}</option>
                                 </select></div>
                             <p>{!! html_entity_decode($product['product_description']) !!} </p>
-                            <p><a href="{{ route('more-info') }}" class="more-info-link">More Info</a></p>
+                            
                         </div>
                     </div>
                 </li>
